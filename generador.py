a1446e1391e56a6de422ff25c555157d4d78970f
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2022-05-06 10:49:53 -0500

    Nuevo

---
+++ generador.py
@@ -0,0 +1,13 @@
+"""import random
+x = []
+for x in range(21):
+    d =random.randint(1,10)
+    x.extend(d)
+print(x)
+"""
+i = 1
+while (i<5):
+    nodo = "texto"
+    nodo = nodo+str(i)
+    print(nodo)
+    i=i+1
